cffb64f7197a1314a2bd43303e7b323c4a234afb
Ladislau Lopes da Silva Júnior <ladislau.contato.01@gmail.com>
2024-10-15 13:09:33 -0500

    crianção do .bat para exclusão(se houver) , criação da venv, ativação dela e instalação dos requirements

---
+++ ambiente_virtuall.bat
@@ -0,0 +1,27 @@
+@echo off
+REM Nome da pasta virtualenv
+set VENV_DIR=venv
+
+REM Verificar se a pasta venv existe e excluí-la
+if exist %VENV_DIR% (
+    echo Excluindo a pasta %VENV_DIR%...
+    rmdir /s /q %VENV_DIR%
+)
+
+REM Criar uma nova virtualenv
+echo Criando uma nova venv...
+python -m venv %VENV_DIR%
+
+REM Ativar a nova venv
+echo Ativando a venv...
+call %VENV_DIR%\Scripts\activate
+
+REM Instalar os requisitos a partir do requirements.txt
+if exist requirements.txt (
+    echo Instalando os requisitos do requirements.txt...
+    pip install -r requirements.txt
+) else (
+    echo Arquivo requirements.txt não encontrado!
+)
+
+echo Processo concluído.
